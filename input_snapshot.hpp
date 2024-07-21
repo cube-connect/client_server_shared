1bf57d10be9ebf38957ad338ddea7beb8541dca9
Junyoung Seok <junyoungseok1@gmail.com>
2024-07-20 21:47:07 -0600

    Added input_snapshot.hpp

---
+++ input_snapshot.hpp
@@ -0,0 +1,17 @@
+struct InputSnapshot
+{
+    bool f_pressed;
+    bool f_prime_pressed;
+    bool b_pressed;
+    bool b_prime_pressed;
+    bool r_pressed;
+    bool r_prime_pressed;
+    bool l_pressed;
+    bool l_prime_pressed;
+    bool u_pressed;
+    bool u_prime_pressed;
+    bool d_pressed;
+    bool d_prime_pressed;
+    double yaw_delta;
+    double pitch_delta;
+};
